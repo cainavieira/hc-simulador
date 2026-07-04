ad1488111872f73839d6f3fb87be72e76237f4ba
Cvieira <cainavt@gmail.com>
2026-07-03 21:07:07 -0300

    feat: adiciona camada de controle e completa a camada de serviço com spring

---
+++ backend/hc/src/main/java/com/caina/hc/controller/TimeInscritoController.java
@@ -0,0 +1,63 @@
+package com.caina.hc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.caina.hc.model.TimeInscrito;
+import com.caina.hc.service.TimeInscritoService;
+
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TimeInscritoController {
+
+    private final TimeInscritoService timeInscritoService;
+
+    public TimeInscritoController(TimeInscritoService timeInscritoService) {
+        this.timeInscritoService = timeInscritoService;
+    }
+
+    @PostMapping("/times")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TimeInscrito addTimeInscrito(@Valid @RequestBody TimeInscrito novoTimeInscrito) {
+        return timeInscritoService.saveTimeInscrito(novoTimeInscrito);
+
+    }
+
+    @GetMapping("/times")
+    @ResponseStatus(HttpStatus.OK)
+    public List<TimeInscrito> getAllInscritos() {
+        return timeInscritoService.getTimesInscritos();
+    }
+
+    @GetMapping("/times/{timeId}")
+    @ResponseStatus(HttpStatus.OK)
+    public TimeInscrito getTimeInscrito(@PathVariable int timeId) {
+        return timeInscritoService.getTimeInscritoById(timeId);
+    }
+
+    @DeleteMapping("/times/{timeId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTimeInscrito(@PathVariable int timeId) {
+        timeInscritoService.deleteTimeById(timeId);
+    }
+
+    @PutMapping("times/{timeId}")
+    public TimeInscrito updateTimeInscrito(@PathVariable int timeId, @RequestBody TimeInscrito novoTimeInscrito) {
+        return timeInscritoService.updateTimeInscrito(timeId, novoTimeInscrito);
+
+    }
+
+}
--- backend/hc/src/main/java/com/caina/hc/service/TimeInscritoService.java
+++ backend/hc/src/main/java/com/caina/hc/service/TimeInscritoService.java
@@ -1,10 +1,13 @@
 package com.caina.hc.service;
+
+
 import org.springframework.stereotype.Service;
 
 import com.caina.hc.model.TimeInscrito;
 import com.caina.hc.repository.TimeInscritoRepository;
 
 import jakarta.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -16,8 +19,36 @@ public class TimeInscritoService {
         this.timeInscritoRepository = timeInscritoRepository;
     }
 
-    public TimeInscrito saveTimeInscrito( TimeInscrito timeInscrito){
-        return timeInscritoRepository.save(timeInscrito);
+    public TimeInscrito saveTimeInscrito(TimeInscrito novoTimeInscrito) {
+        return timeInscritoRepository.save(novoTimeInscrito);
+    }
+
+    public void deleteTimeById(int timeInscritoId) {
+        if (!timeInscritoRepository.existsById(timeInscritoId)) {
+            return;
+        }
+        timeInscritoRepository.deleteById(timeInscritoId);
+    }
+
+    public TimeInscrito getTimeInscritoById(int timeInscritoId) {
+        return timeInscritoRepository.findById(timeInscritoId)
+                .orElseThrow(() -> new RuntimeException());
+    }
+
+    public List<TimeInscrito> getTimesInscritos() {
+        return timeInscritoRepository.findAll();
+    }
+
+    public TimeInscrito updateTimeInscrito(int novoInscritoId, TimeInscrito novoTimeInscrito) {
+        if (!timeInscritoRepository.existsById(novoInscritoId)) {
+            throw new RuntimeException();
+        }
+        novoTimeInscrito.setNomeJogador(novoTimeInscrito.getNomeJogador());
+        novoTimeInscrito.setTimeJogador(novoTimeInscrito.getTimeJogador());
+
+        novoTimeInscrito.setId(novoInscritoId);
+        return timeInscritoRepository.save(novoTimeInscrito);
+
     }
 
 }
