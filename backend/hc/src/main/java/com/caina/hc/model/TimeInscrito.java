624ec3be48d4b9e5ff99f41b6dcccf3e5878f587
Cvieira <cainavt@gmail.com>
2026-07-04 01:46:23 -0300

    chore: altera a entidade TimeInscrio por causa de erros no POST

--- backend/hc/src/main/java/com/caina/hc/model/TimeInscrito.java
+++ backend/hc/src/main/java/com/caina/hc/model/TimeInscrito.java
@@ -6,28 +6,31 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
-@Data public class TimeInscrito {
+@Data
+public class TimeInscrito {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotEmpty
-    @Column(updatable = false)
     private String nomeJogador;
 
     @NotEmpty
-    @Column(updatable = false)
     private String timeJogador;
 
     @NotEmpty
-    @Column(updatable = false)
     private String codigoPais;
+
+    public TimeInscrito(String nome, String time, String codigo) {
+        this.nomeJogador = nome;
+        this.timeJogador = time;
+        this.codigoPais = codigo;
+    }
 }
