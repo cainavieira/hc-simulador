541a928bf76c03661f66eb5e114a720c4baa4456
Cvieira <cainavt@gmail.com>
2026-07-04 12:56:16 -0300

    chore: atualiza o git ignore

--- backend/hc/src/main/java/com/caina/hc/repository/TimeInscritoRepository.java
+++ backend/hc/src/main/java/com/caina/hc/repository/TimeInscritoRepository.java
@@ -3,7 +3,7 @@ package com.caina.hc.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 
 import com.caina.hc.model.TimeInscrito;
 
